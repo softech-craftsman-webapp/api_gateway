576ad8ef44c83b131bb60e1eca52a07baa38616b
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-10-15 02:12:23 +0200

    changes applied

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,7 +15,7 @@ const options = {
   ws: false,
   toProxy: false,
   xfwd: false,
-  secure: false,
+  secure: true,
   changeOrigin: false,
   preserveHeaderKeyCase: true,
   proxyTimeout: 13000,
@@ -50,6 +50,26 @@ proxy.on('error', (_err, _req, res) => {
 */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 proxy.on('proxyReq', (_proxyReq, req, res, _options)  => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, PUT, POST, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Access-Control-Allow-Origin,X-Request-ID,X-Requested-With,Content-Type,Authorization,Accept,Origin,Access-Control-Max-Age');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+  }
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+proxy.on('proxyRes', (_proxyReq, req, res, _options)  => {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
 
